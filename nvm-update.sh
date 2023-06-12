c5f9c1c4b7aaa6420254bf400f05c285791b18b9
Donavon Buchanan <5569820+dbuchanandev@users.noreply.github.com>
2023-06-11 21:41:31 -0500

    Check install before migration
    Determine that the newly installed version works before migrating and cleaning up packages and old installations.

--- nvm-update.sh
+++ nvm-update.sh
@@ -47,43 +47,55 @@ fi
 # homebridge directory location for check below
 HB_DIR=$HOME'/.homebridge'
 
-# current local version
-CURRENT="$(nvm current)"
-# latest LTS
-LTS="$(nvm version-remote --lts)"
-# Check if update is needed
-if [ "$CURRENT" == "$LTS" ]; then
-    # exit
-    echo ""
-    echo "Current version $CURRENT matches latest $LTS."
-    echo "No update needed."
-    exit 0
-else
-    
+reinstall_homebridge() {
     # Handle Homebridge Installation
     if [ -d "$HB_DIR" ]; then
-        echo "Homebridge installation found."
-        echo "Stopping Homebridge during update."
-        # Stop Homebridge during update
-        sudo hb-service stop
-        sudo hb-service uninstall
-    fi
-    
-    # update and reinstall packages
-    echo "Updating from $CURRENT to $LTS."
-    nvm install "lts/*" --reinstall-packages-from=$CURRENT
-    
-    # Handle Homebridge Installation
-    if [ -d "$HB_DIR" ]; then
-        # Restart Homebrige after update
         echo "Reinstalling Homebridge service."
         sudo hb-service rebuild --all
         sudo hb-service install
         sudo hb-service start
     fi
+}
+
+# current local version
+CURRENT="$(nvm current)"
+# latest LTS
+LTS="$(nvm version-remote --lts)"
+
+# Check if update is needed
+if [ "$CURRENT" == "$LTS" ]; then
+    echo "Current version $CURRENT matches latest $LTS."
+    echo "No update needed."
+    exit 0
+else
+    # Handle Homebridge Installation
+    if [ -d "$HB_DIR" ]; then
+        echo "Homebridge installation found."
+        echo "Stopping Homebridge during update."
+        sudo hb-service stop
+        sudo hb-service uninstall
+    fi
     
-    # cleanup old install
-    nvm uninstall $CURRENT
+    # Only install new version without migrating packages
+    echo "Installing new version: $LTS."
+    nvm install "lts/*"
+    
+    # Test the new node version by running a simple command
+    # 'node -v' should print out the version number of the node installation
+    node -v > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Error with the new node version. Rolling back to $CURRENT."
+        nvm uninstall lts/*
+        nvm use $CURRENT
+    else
+        # Now that new version is confirmed working, migrate packages
+        echo "Migrating packages from $CURRENT to $LTS."
+        nvm reinstall-packages $CURRENT
+
+        # cleanup old install
+        nvm uninstall $CURRENT
+    fi
+    reinstall_homebridge
 fi
 
 exit 0
